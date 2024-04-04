cb2d3f2b17d6aaa5ef56c97981a5191aa8a516b8
sharaf8 <niyozovsharaf@gmail.com>
2024-04-04 13:25:33 +0300

    https://www.codewars.com/kata/529f2d1c403a58f660000656/train/javascript

---
+++ kata-7/Basic JS - Building a calculator.js
@@ -0,0 +1,8 @@
+var Calculator = {
+    add: function(a, b) { return a + b},
+    subtract: function(a, b) { return a - b},
+    multiply: function(a, b) {return a * b},
+    divide: function(a, b) {
+        if(b === 0) return false;
+        return a / b}
+};
