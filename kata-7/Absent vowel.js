b99c0268c81e487f85e0b1a2e209494d9bf27602
sharaf8 <niyozovsharaf@gmail.com>
2024-04-08 18:33:40 +0300

    https://www.codewars.com/kata/56414fdc6488ee99db00002c/train/javascript

---
+++ kata-7/Absent vowel.js
@@ -0,0 +1,8 @@
+function absentVowel(x){
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    for(let i = 0; i < x.length; i++){
+        if(!x.includes(vowels[i])){
+            return i;
+        }
+    }
+}
