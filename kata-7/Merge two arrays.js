0e2b155fc7f25f1a79c857681829eea68ad9967c
sharaf8 <niyozovsharaf@gmail.com>
2024-04-09 17:46:50 +0300

    https://www.codewars.com/kata/583af10620dda4da270000c5/train/javascript

---
+++ kata-7/Merge two arrays.js
@@ -0,0 +1,15 @@
+function mergeArrays(arr1, arr2) {
+    const result = [];
+    const maxLength = Math.max(arr1.length, arr2.length);
+
+    for (let i = 0; i < maxLength; i++) {
+        if (i < arr1.length) {
+            result.push(arr1[i]);
+        }
+        if (i < arr2.length) {
+            result.push(arr2[i]);
+        }
+    }
+
+    return result;
+}
