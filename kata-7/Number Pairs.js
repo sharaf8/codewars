afc40d0aff3c07a8da13b1f194728427aada8c03
sharaf8 <niyozovsharaf@gmail.com>
2024-04-24 19:39:00 +0300

    Delete Number Pairs.js

--- kata-7/Number Pairs.js
+++
@@ -1,9 +0,0 @@
-function getLargerNumbers(arr1, arr2) {
-    const result = [];
-
-    for (let i = 0; i < arr1.length; i++) {
-        result.push(Math.max(arr1[i], arr2[i]));
-    }
-
-    return result;
-}
