c6cb8d067fd9800ead0b8e5e6daf3c49c206a655
sharaf8 <niyozovsharaf@gmail.com>
2024-04-26 19:53:06 +0300

    Remove unnecessary files

--- kata-7/Elevator Distance.js
+++
@@ -1,7 +0,0 @@
-function elevatorDistance(array) {
-    let result = 0;
-    for (let i = 0; i < array.length - 1; i++){
-        result += Math.abs(array[i] - array[i + 1]);
-    }
-    return result;
-}
