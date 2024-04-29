2657e103e3e75bdb92f50b8f3c3d73d45859b614
sharaf8 <niyozovsharaf@gmail.com>
2024-04-29 20:59:57 +0300

    https://www.codewars.com/kata/573156709a231dcec9000ee8/train/javascript

--- kata-7/methods of arrayObject---map().js
+++
@@ -1,8 +0,0 @@
-function isolateIt(arr){
-    return arr.map(str => {
-        const mid = Math.floor(str.length / 2);
-        return str.length % 2 === 0 ?
-            str.slice(0, mid) + "|" + str.slice(mid) :
-            str.slice(0, mid) + "|" + str.slice(mid + 1);
-    })
-}
