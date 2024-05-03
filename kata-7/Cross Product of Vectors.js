b84c82ef37beea1d9fc88461dafd585837731245
sharaf8 <niyozovsharaf@gmail.com>
2024-05-03 21:30:46 +0300

    https://www.codewars.com/kata/54566695309908a6590005f1/train/javascript

---
+++ kata-7/Cross Product of Vectors.js
@@ -0,0 +1,11 @@
+function crossProduct(vector1, vector2) {
+    if (!Array.isArray(vector1) || vector1.length !== 3 || !Array.isArray(vector2) || vector2.length !== 3) {
+        throw "Arguments are not 3D vectors!";
+    }
+
+    const x = vector1[1] * vector2[2] - vector1[2] * vector2[1];
+    const y = vector1[2] * vector2[0] - vector1[0] * vector2[2];
+    const z = vector1[0] * vector2[1] - vector1[1] * vector2[0];
+
+    return [x, y, z];
+}
