0125aab104b803d4cb5160d100464712bb3161b7
sharaf8 <niyozovsharaf@gmail.com>
2024-05-07 18:52:48 +0300

    https://www.codewars.com/kata/59727ff285281a44e3000011/train/javascript

---
+++ kata-7/Band name generator.js
@@ -0,0 +1,8 @@
+function bandNameGenerator(str) {
+    if (str[0] === str[str.length - 1]) {
+        return str[0].toUpperCase() + str.slice(1) + str.slice(1);
+    }
+    else {
+        return "The " + str[0].toUpperCase() + str.slice(1);
+    }
+}
