f08c303b804f15086a8cca8d3ec050aa5115958b
sharaf8 <niyozovsharaf@gmail.com>
2024-05-10 19:48:16 +0300

    Deleted Convert string to camel case.js

--- kata-6/Convert string to camel case.js
+++
@@ -1,9 +0,0 @@
-function toCamelCase(str) {
-    let words = str.split(/[-_]/);
-
-    for (let i = 1; i < words.length; i++) {
-        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
-    }
-
-    return words.join('');
-}
