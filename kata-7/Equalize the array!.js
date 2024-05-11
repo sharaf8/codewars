c98840baf7f670a0aed1031998166861fa64926c
sharaf8 <niyozovsharaf@gmail.com>
2024-05-11 20:12:19 +0300

    Deleted Equalize the array!.js

--- kata-7/Equalize the array!.js
+++
@@ -1,7 +0,0 @@
-function equalize(array){
-    const firstEl = array[0];
-    return array.map(num => {
-        const minus = num - firstEl;
-        return (minus >= 0 ? "+" : "-") + Math.abs(minus);
-    });
-}
