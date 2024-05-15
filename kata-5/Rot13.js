cfadd2e20e15cb55a14abb6835774350dce78a91
sharaf8 <niyozovsharaf@gmail.com>
2024-05-15 21:23:02 +0300

    https://www.codewars.com/kata/530e15517bc88ac656000716/train/javascript

---
+++ kata-5/Rot13.js
@@ -0,0 +1,7 @@
+function rot13(str) {
+    return str.replace(/[a-zA-Z]/g, function(char) {
+        var charCode = char.charCodeAt(0);
+        var offset = charCode <= 90 ? 65 : 97;
+        return String.fromCharCode(((charCode - offset + 13) % 26) + offset);
+    });
+}
