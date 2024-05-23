3dd675261c95934a4a9a60baf1a0c0b3d4ca3dea
sharaf8 <niyozovsharaf@gmail.com>
2024-05-23 18:44:10 +0300

    https://www.codewars.com/kata/5324945e2ece5e1f32000370/train/javascript

--- kata-4/Sum Strings as Numbers.js
+++ kata-4/Sum Strings as Numbers.js
@@ -1,21 +1,3 @@
-function sumStrings(a, b) {
-    function addStrings(str1, str2) {
-        let result = '';
-        let carry = 0;
-
-        for (let i = str1.length - 1, j = str2.length - 1; i >= 0 || j >= 0 || carry > 0; i--, j--) {
-            const digit1 = i >= 0 ? parseInt(str1[i]) : 0;
-            const digit2 = j >= 0 ? parseInt(str2[j]) : 0;
-            const sum = digit1 + digit2 + carry;
-            result = (sum % 10) + result;
-            carry = Math.floor(sum / 10);
-        }
-
-        return result;
-    }
-
-    a = a.replace(/^0+/, '');
-    b = b.replace(/^0+/, '');
-
-    return addStrings(a, b);
+function sumStrings(a,b) {
+    return (BigInt(a) + BigInt(b)).toString();
 }
