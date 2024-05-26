45c2a24a78b3eb4acc85dd20848c512dbe873969
sharaf8 <niyozovsharaf@gmail.com>
2024-05-26 19:20:23 +0300

    https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/train/javascript

---
+++ kata-6/Multiplication table.js
@@ -0,0 +1,11 @@
+multiplicationTable = function(size) {
+    const result = [];
+    for (let i = 1; i <= size; i++) {
+        const row = [];
+        for (let j = 1; j <= size; j++) {
+            row.push(i * j);
+        }
+        result.push(row);
+    }
+    return result
+}
