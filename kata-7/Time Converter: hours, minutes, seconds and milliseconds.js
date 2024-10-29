bdd5080fca8b22c81b04aa8da29d2b02ffbb90b0
Sharaf Niyozov <156796191+sharaf8@users.noreply.github.com>
2024-10-29 20:57:20 +0500

    https://www.codewars.com/kata/56b8b0ae1d36bb86b2000eaa/train/javascript

---
+++ kata-7/Time Converter: hours, minutes, seconds and milliseconds.js
@@ -0,0 +1,8 @@
+function convert(date) {
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
+
+    return `${hours}:${minutes}:${seconds},${milliseconds}`;
+}
