2c171aea3909b665f9066ed80e1f99455daa357c
Sharaf8 <niyozovsharaf@gmail.com>
2025-04-02 09:21:24 +0500

    New update

--- kata-8/Even or Odd.js
+++ kata-8/Even or Odd.js
@@ -2,6 +2,6 @@ function evenOrOdd(number) {
     return number % 2 === 0  ? "Even" : "Odd";
 }
 
-/*
-    const
- */
+// const evenOrOdd = n => n % 2 === 0 ? "Even" : "Odd";
+
+console.log(evenOrOdd(21.1))
